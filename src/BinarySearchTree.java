dd5d07e09ec120775dbe2a307f070c9dcfe8ffa6
Brett Mainers <brettmainers@gmail.com>
2025-02-20 09:06:57 -0800

    Fixed a few bugs

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -5,16 +5,14 @@ public class BinarySearchTree {
     public void addNode(int val) {
         if (root == null) {
             root = new Node(val);
-        } else if (val < root.val){
-            addNodeHelper(root.leftChild,val);
         } else {
-            addNodeHelper(root.rightChild,val);
+            addNodeHelper(root, val);
         }
     }
 
-    private static Node addNodeHelper(Node node, int val) {
+    private Node addNodeHelper(Node node, int val) {
         if (node == null) {
-            new Node(val);
+            return new Node(val);
         } else if (val < node.val) {
             node.leftChild = addNodeHelper(node.leftChild, val);
         } else {
@@ -22,5 +20,97 @@ public class BinarySearchTree {
         }
         return node;
     }
-}
 
+    public boolean search(int val) {
+        return searchHelper(root, val);
+    }
+
+    private boolean searchHelper(Node node, int val) {
+        if (node == null) {
+            return false;
+        } else if (val == node.val) {
+            return true;
+        } else if (val < node.val) {
+            return searchHelper(node.leftChild, val);
+        } else {
+            return searchHelper(node.rightChild, val);
+        }
+    }
+
+    public void removeNode(int val) {
+        root = removeNodeHelper(root, val);
+    }
+
+    private Node removeNodeHelper(Node node, int val) {
+        if (node == null) {
+            return null;
+        }
+
+        if (val < node.val) {
+            node.leftChild = removeNodeHelper(node.leftChild, val);
+        } else if (val > node.val) {
+            node.rightChild = removeNodeHelper(node.rightChild, val);
+        } else {
+            if (node.leftChild == null) {
+                return node.rightChild;
+            } else if (node.rightChild == null) {
+                return node.leftChild;
+            }
+
+            node.val = findMinValue(node.rightChild);
+
+
+            node.rightChild = removeNodeHelper(node.rightChild, node.val);
+        }
+
+        return node;
+    }
+    
+    private int findMinValue(Node node) {
+        int minValue = node.val;
+        while (node.leftChild != null) {
+            minValue = node.leftChild.val;
+            node = node.leftChild;
+        }
+        return minValue;
+    }
+
+    public void inorderTraversal() {
+        inorderHelper(root);
+        System.out.println();
+    }
+
+    private void inorderHelper(Node node) {
+        if (node != null) {
+            inorderHelper(node.leftChild);
+            System.out.print(node.val + " ");
+            inorderHelper(node.rightChild);
+        }
+    }
+
+    public void preorderTraversal() {
+        preorderHelper(root);
+        System.out.println();
+    }
+
+    private void preorderHelper(Node node) {
+        if (node != null) {
+            System.out.print(node.val + " ");
+            preorderHelper(node.leftChild);
+            preorderHelper(node.rightChild);
+        }
+    }
+
+    public void postorderTraversal() {
+        postorderHelper(root);
+        System.out.println();
+    }
+
+    private void postorderHelper(Node node) {
+        if (node != null) {
+            postorderHelper(node.leftChild);
+            postorderHelper(node.rightChild);
+            System.out.print(node.val + " ");
+        }
+    }
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,37 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        
-        BinarySearchTree bst = new BinarySearchTree();
 
+        BinarySearchTree bst = new BinarySearchTree();
         bst.addNode(10);
+        bst.addNode(5);
+        bst.addNode(15);
+        bst.addNode(3);
+        bst.addNode(7);
+        bst.addNode(12);
+        bst.addNode(18);
+
+
+        System.out.println("Inorder traversal:");
+        bst.inorderTraversal();
+        System.out.println();
+
+        System.out.println("Preorder traversal:");
+        bst.preorderTraversal();
+        System.out.println();
+
+        System.out.println("Postorder traversal:");
+        bst.postorderTraversal();
+        System.out.println();
+
+        System.out.println("Searching for 7: " + bst.search(7));
+        System.out.println("Searching for 20: " + bst.search(20));
+        System.out.println();
+
+        System.out.println("Removing 10");
+        System.out.println();
+        bst.removeNode(10);
+        System.out.println("Inorder traversal after removing 10:");
+        bst.inorderTraversal();
+
     }
 }
